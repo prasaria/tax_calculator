8dfd633ff7883a94617ed8e669dc8c311db028b6
prasaria <prasetya.ariawibawa@gmail.com>
2018-11-23 06:32:49 +0700

    configure bullet gem in development environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+  Bullet.enable = true
+  Bullet.bullet_logger = true
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
